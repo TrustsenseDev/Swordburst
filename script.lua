85d1c56ba1bd3379acfb7d5985ad98a7a6299a3f
Trustsense <98208313+TrustsenseDev@users.noreply.github.com>
2024-02-23 17:50:00 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -105,7 +105,7 @@ local toolkit = {} do
 
         coroutine.wrap(function()
             while Settings.autofarm do
-                toolkit.attack({target})
+                toolkit.attack(target)
                 task.wait(0.5)
             end
         end)()
